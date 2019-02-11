f6fb2385dc0b1340fffb24781cf30beb54ea9c20
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-11 16:07:31 +0000

    SCARV: Add script to build SCARV-only toolchain
    - Target RV32imaxc architecture only
    
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .travis.yml
    	new file:   build-rv32imaxc.sh

---
+++ build-rv32imaxc.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# XCrypto specific script to build only the toolchain we really care
+# about using.
+#
+# Script to build RISC-V ISA simulator, proxy kernel, and GNU toolchain.
+# Tools will be installed to $RISCV.
+
+. build.common
+
+echo "Starting RISC-V Toolchain build process"
+
+build_project riscv-fesvr --prefix=$RISCV
+build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV --with-isa=rv32imaxc
+build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=rv32imaxc --with-abi=ilp32
+CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf
+build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --disable-werror
+
+echo -e "\\nRISC-V Toolchain installation completed!"
