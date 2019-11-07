fe3c6776250e2ecfeaa96be56fce670daa9f173d
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-11-07 10:24:25 +0000

    SCARV: Bump submodules for XCrypto v1.0.0
     On branch scarv/xcrypto/v1.0.0
     Your branch is up-to-date with 'origin/scarv/xcrypto/v1.0.0'.
    
     Changes to be committed:
    	modified:   build-rv32imaxc.sh
    	modified:   build.common
    	modified:   riscv-gnu-toolchain
    	modified:   riscv-isa-sim
    	modified:   riscv-opcodes

--- build-rv32imaxc.sh
+++ build-rv32imaxc.sh
@@ -11,9 +11,9 @@
 echo "Starting RISC-V Toolchain build process"
 
 build_project riscv-fesvr --prefix=$RISCV
-build_project riscv-isa-sim --prefix=$RISCV --with-isa=rv32imaxc
-build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=rv32imaxc --with-abi=ilp32
-CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf --with-arch=rv32imaxc
+build_project riscv-isa-sim --prefix=$RISCV --with-isa=rv32imac
+build_project riscv-gnu-toolchain --prefix=$RISCV --with-arch=rv32imac --with-abi=ilp32
+CC= CXX= build_project riscv-pk --prefix=$RISCV --host=riscv32-unknown-elf --with-arch=rv32imac
 build_project riscv-openocd --prefix=$RISCV --enable-remote-bitbang --disable-werror
 
 echo -e "\\nRISC-V Toolchain installation completed!"
